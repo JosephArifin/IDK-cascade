1186aee56c0257fb1dc594d9c528ed034b9c4a12
JosephArifin <josepharifin03@gmail.com>
2025-06-16 12:21:43 -0500

    update: shortened getting paths

--- ImageNetDataset.py
+++ ImageNetDataset.py
@@ -13,21 +13,18 @@ class ImageNetDataset(Dataset):
     def __init__(self, data_dir, transform) -> None:
         self.data_dir = data_dir
         self.transform = transform
-        self.paths: list[str] = []
+        self.paths: list[tuple] = []
 
         for c in range(1000):
             class_dir = os.path.join(data_dir, str(c))
-            image_dirs = [
-                image_dir
-                for image_dir in os.listdir(class_dir)
-                if image_dir.endswith(".jpeg")
-            ]
-            self.paths.extend(os.path.join(class_dir, p) for p in image_dirs)
+            for p in os.listdir(class_dir):
+                if p.endswith(".jpeg"):
+                    self.paths.append((os.path.join(class_dir, p), str(c)))
 
     def __len__(self):
         return len(self.paths)
 
     def __getitem__(self, idx):
-        input_image = Image.open(self.paths[idx]).convert("RGB")
+        input_image = Image.open(self.paths[idx][0])
         input_tensor = self.transform(input_image)
         return input_tensor
