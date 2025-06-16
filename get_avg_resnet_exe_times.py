39c5899466b452fcb3274ee998b42b9feda23c55
JosephArifin <josepharifin03@gmail.com>
2025-06-16 12:23:23 -0500

    update: removed redundant batch counter

--- get_avg_resnet_exe_times.py
+++ get_avg_resnet_exe_times.py
@@ -21,14 +21,13 @@ avg_exe_time = {
 }
 
 dataset = ImageNetDataset.ImageNetDataset(data_dir, preprocess)
-dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0)
+dataloader = DataLoader(dataset, batch_size=16, shuffle=False, num_workers=4)
 
 with open("imagenet_classes.txt", "r") as f:
     categories = [line.strip() for line in f.readlines()]
 
 for layers, model in models_dict.items():
     total_time = 0.0
-    total_batches = 0
 
     for batch in dataloader:
         batch = batch.to(device)
@@ -38,7 +37,6 @@ for layers, model in models_dict.items():
             output = model(batch)
         end_time = time.perf_counter()
         total_time += end_time - start_time
-        total_batches += 1
 
     avg_exe_time[layers] = (total_time / len(dataset)) * 1000
 print(avg_exe_time)
